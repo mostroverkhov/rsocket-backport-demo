b04a704cb0d2f54f074923c17c3201cafe247b7c
Masksym Ostroverkhov <m.ostroverkhov@gmail.com>
2017-11-02 16:19:54 +0200

    make rsocket client robust to connectivity changes
    set rxjava2 default error handler - seems like rsocket-core does not handle connection errors

--- app/src/main/java/com/github/mostroverkhov/rsocket/backport/sample/App.kt
+++ app/src/main/java/com/github/mostroverkhov/rsocket/backport/sample/App.kt
@@ -3,6 +3,8 @@ package com.github.mostroverkhov.rsocket.backport.sample
 import android.app.Application
 import android.support.annotation.IntegerRes
 import android.support.annotation.StringRes
+import android.util.Log
+import io.reactivex.plugins.RxJavaPlugins
 
 /**
  * Created by Maksym Ostroverkhov
@@ -11,6 +13,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         context = this
+        RxJavaPlugins.setErrorHandler { Log.e(javaClass.simpleName, "uncaught error: ${it.message}") }
     }
 
     companion object {
--- app/src/main/java/com/github/mostroverkhov/rsocket/backport/sample/RSocketClient.kt
+++ app/src/main/java/com/github/mostroverkhov/rsocket/backport/sample/RSocketClient.kt
@@ -1,11 +1,17 @@
 package com.github.mostroverkhov.rsocket.backport.sample
 
-import g.io.rsocket.transport.client.OkhttpWebsocketClientTransport
+import com.github.mostroverkhov.rsocket.backport.transport.okhttp.client.OkhttpWebsocketClientTransport
+import io.reactivex.Completable
+import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.processors.AsyncProcessor
 import io.reactivex.schedulers.Schedulers
+import io.rsocket.DuplexConnection
+import io.rsocket.Frame
 import io.rsocket.RSocket
 import io.rsocket.RSocketFactory
+import io.rsocket.plugins.DuplexConnectionInterceptor
+import org.reactivestreams.Publisher
 import java.util.concurrent.atomic.AtomicReference
 
 /**
@@ -18,6 +24,7 @@ class RSocketClient(protocol: String,
     private val tryRSocket: AtomicReference<AsyncProcessor<RSocket>> = AtomicReference()
     private val rSocket: Single<RSocket> = RSocketFactory
             .connect()
+            .addConnectionPlugin(SourceConnectionInterceptor { ResetRSocketOnError(it) })
             .acceptor { -> responder }
             .transport(OkhttpWebsocketClientTransport
                     .create(protocol, host, port))
@@ -32,11 +39,11 @@ class RSocketClient(protocol: String,
         } else {
             tryRSocket.get().firstOrError()
         }
-    }
+    }.subscribeOn(Schedulers.io())
 
     private fun memoizeSuccess(): Single<RSocket> {
         val rSocket = tryRSocket.get()
-        this.rSocket.observeOn(Schedulers.io()).subscribe(
+        this.rSocket.subscribe(
                 { rs ->
                     rSocket.onNext(rs)
                     rSocket.onComplete()
@@ -46,6 +53,27 @@ class RSocketClient(protocol: String,
                     tryRSocket.set(null)
                 })
         return rSocket.firstOrError()
+    }
+
+    private inner class ResetRSocketOnError(private val conn: DuplexConnection) : DuplexConnection {
+        override fun close(): Completable = conn.close()
+
+        override fun availability(): Double = conn.availability()
+
+        override fun onClose(): Completable = conn.onClose()
+
+        override fun receive(): Flowable<Frame> = conn.receive().doOnError { _ -> tryRSocket.set(null) }
+
+        override fun send(frame: Publisher<Frame>): Completable = conn.send(frame).doOnError { tryRSocket.set(null) }
+
+    }
+
+    private class SourceConnectionInterceptor(
+            private val interceptor: (DuplexConnection) -> DuplexConnection)
+        : DuplexConnectionInterceptor {
+        override fun invoke(type: DuplexConnectionInterceptor.Type,
+                            conn: DuplexConnection): DuplexConnection =
+                if (type == DuplexConnectionInterceptor.Type.SOURCE) interceptor(conn) else conn
     }
 }
 
