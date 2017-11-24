ebfbb1936433c7a508ca640fbc3368257d02d8d2
Masksym Ostroverkhov <m.ostroverkhov@gmail.com>
2017-11-24 16:01:32 +0200

    remove global error handler

--- app/src/main/java/com/github/mostroverkhov/rsocket/backport/sample/App.kt
+++ app/src/main/java/com/github/mostroverkhov/rsocket/backport/sample/App.kt
@@ -1,10 +1,6 @@
 package com.github.mostroverkhov.rsocket.backport.sample
 
 import android.app.Application
-import android.support.annotation.IntegerRes
-import android.support.annotation.StringRes
-import android.util.Log
-import io.reactivex.plugins.RxJavaPlugins
 
 /**
  * Created by Maksym Ostroverkhov
@@ -13,7 +9,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         context = this
-        RxJavaPlugins.setErrorHandler { Log.e(javaClass.simpleName, "uncaught error: ${it.message}") }
     }
 
     companion object {
